567b32bb06fc68ce5e658e45a7d5397c0626cfab
Mini Model <minimodel@metamodel.me>
2013-05-13 23:11:55 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodelsattribute_timestamp/languages/el/tl_metamodel_attribute.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/el/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-13T22:14:47+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timesettings_legend']      = 'Ημερομηνια και ωρα';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timetype']['0']            = 'Σχημα';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timetype']['1']            = 'Εδώ μπορείτε να επιλέξετε το επιθυμητό σχήμα.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timetypeOptions']['date']  = 'Ημερομηνια';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timetypeOptions']['datim'] = 'Ημερομηνια και ωρα';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['date']      = 'Ημερομηνια';
