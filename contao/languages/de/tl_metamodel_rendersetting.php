a25a864809ea86df84dd2ea652c97c5728ec7fd2
Mini Model <minimodel@metamodel.me>
2015-09-18 10:33:11 +0200

    Updated translations from transifex.

---
+++ contao/languages/de/tl_metamodel_rendersetting.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-11T04:12:39+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timeformat']['0']     = 'Format';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timeformat']['1']     = 'Hier können Sie ein angepasstes Datumsformat festlegen, Falls Sie dieses Feld leer lassen wird der für das System festgelegte Standardwert benutzt. Der Formatstring wird mit der date()-Funktion von PHP erzeugt.';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timesettings_legend'] = 'Datums- und Zeiteinstellungen';
+
---
+++ contao/languages/ru/tl_metamodel_rendersetting.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-12-15T12:15:05+01:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timeformat']['0']     = 'Формат';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timeformat']['1']     = 'Здесь можно определить формат даты. Если оставить поле пустым, будет использовано значение по умолчанию. Строка формата будет анализирована с помощью функции PHP date().';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timesettings_legend'] = 'Настройки даты и времени';
+
