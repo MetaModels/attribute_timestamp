894e3f5f80426b415f8ebee4a9ed75abedbab531
Mini Model <minimodel@metamodel.me>
2016-03-21 00:18:45 +0100

    Updated translations from transifex.

---
+++ contao/languages/fr/tl_metamodel_rendersetting.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-11T04:12:39+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timeformat']['0']     = 'Formatage';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timeformat']['1']     = 'Vous pouvez définir un format de date personnalisé. Si laissez vide, le format par défaut sera utilisé. Le format sera analysé selon la fonction PHP date().';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timesettings_legend'] = 'Réglages de date et d\'heure';
+
