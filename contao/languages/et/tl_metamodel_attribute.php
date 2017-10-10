8d3949cc0065fdf525df3d2ee8264cad73d7218c
Mini Model <minimodel@metamodel.me>
2017-10-10 13:07:35 +0200

    Updated translations from transifex.

---
+++ contao/languages/et/tl_metamodel_attribute.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
+ *
+ * last-updated: 2017-10-10T13:07:28+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timesettings_legend']      = 'Kuupäev ja kellaaeg';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timetype']['0']            = 'Skeem';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timetypeOptions']['date']  = 'Kuupäev';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timetypeOptions']['datim'] = 'Kuupäev ja kellaaeg';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['timetypeOptions']['time']  = 'Kellaaeg';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['timestamp'] = 'Kuupäev';
+
