32a50614b2a588bb157485549c7020b81edc0ac1
Mini Model <minimodel@metamodel.me>
2019-01-03 20:21:47 +0100

    Update PHPDoc file headers

--- src/MetaModels/Attribute/Timestamp/Timestamp.php
+++ src/MetaModels/Attribute/Timestamp/Timestamp.php
@@ -3,15 +3,14 @@
 /**
  * This file is part of MetaModels/attribute_timestamp.
  *
- * (c) 2012-2018 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeTimestamp
+ * @package    MetaModels/attribute_timestamp
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Andreas Isaak <info@andreas-isaak.de>
@@ -21,8 +20,8 @@
  * @author     Henry Lamorski <henry.lamorski@mailbox.org>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2018 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_timestamp/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_timestamp/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
