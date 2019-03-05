95b046c745cc51d460ad6c92bf17dc92e821fe3a
Mini Model <minimodel@metamodel.me>
2019-03-05 19:40:16 +0100

    Update PHPDoc file headers

--- src/deprecated-autoload.php
+++ src/deprecated-autoload.php
@@ -3,18 +3,17 @@
 /**
  * This file is part of MetaModels/attribute_timestamp.
  *
- * (c) 2012-2017 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeTimestamp
+ * @package    MetaModels/attribute_timestamp
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2012-2017 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_text/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_timestamp/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
