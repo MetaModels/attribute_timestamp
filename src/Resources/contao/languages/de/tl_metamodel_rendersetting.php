ded76b193a965afee729e257b67cfb823681776a
Mini Model <minimodel@metamodel.me>
2020-02-26 20:26:51 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_rendersetting.php
+++ src/Resources/contao/languages/de/tl_metamodel_rendersetting.php
@@ -1,18 +1,27 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_timestamp.
+ *
+ * (c) 2012-2020 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ * Last-updated: 2020-02-26T20:26:46+01:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2015-07-11T04:12:39+02:00
+ * @copyright 2012-2020 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_timestamp/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
 
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timeformat']['0']     = 'Format';
-$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timeformat']['1']     = 'Hier können Sie ein angepasstes Datumsformat festlegen, Falls Sie dieses Feld leer lassen wird der für das System festgelegte Standardwert benutzt. Der Formatstring wird mit der date()-Funktion von PHP erzeugt.';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timeformat']['1']     = 'Hier können Sie ein angepasstes Datumsformat festlegen. Falls Sie dieses Feld leer lassen, wird der für das System festgelegte Standardwert benutzt. Der Formatstring wird mit der date()-Funktion von PHP erzeugt.';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['timesettings_legend'] = 'Datums- und Zeiteinstellungen';
 
