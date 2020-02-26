c27ad97c28a659f8be1ee372af0ef053f6478a95
Stefan Heimes <stefan_heimes@hotmail.com>
2020-02-26 22:16:06 +0100

    Add a missing author

--- tests/Attribute/TimestampTest.php
+++ tests/Attribute/TimestampTest.php
@@ -16,6 +16,7 @@
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_timestamp/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
