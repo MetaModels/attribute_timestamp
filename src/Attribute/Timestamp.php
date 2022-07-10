2720e6694ccd03e7627a0c5551214cf63b862e52
Ingolf Steinhardt <info@e-spin.de>
2022-07-10 15:34:41 +0200

    Fix PHP 8 warning

--- src/Attribute/Timestamp.php
+++ src/Attribute/Timestamp.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_timestamp.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -20,7 +20,7 @@
  * @author     Henry Lamorski <henry.lamorski@mailbox.org>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2021 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_timestamp/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -105,7 +105,7 @@ class Timestamp extends Numeric
 
         if (empty($arrFieldDef['eval']['readonly'])) {
             $arrFieldDef['eval']['datepicker'] = true;
-            $arrFieldDef['eval']['tl_class']  .= ' wizard';
+            $arrFieldDef['eval']['tl_class']   = $arrFieldDef['eval']['tl_class'] ?? '' . ' wizard';
         }
         $arrFieldDef['eval']['clear_datetime'] = ($arrOverrides['clear_datetime'] ?? null);
 
